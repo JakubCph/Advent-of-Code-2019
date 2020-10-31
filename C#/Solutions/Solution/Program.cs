62cb77550bcbc32f585ab33601785a78870b89bf
JakubCph <38160647+JakubCph@users.noreply.github.com>
2020-10-31 17:03:06 +0100

    Created solution to Day1 part 1 problem in C#. Added .gitignore.

---
+++ C#/Solutions/Solution/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace Solution
+{
+    class Program
+    {
+        private const string Path = "Input.txt";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string line;
+            int fuel = 0;
+            StreamReader file = Reader.OpenStream(Path);
+
+            while ((line = file.ReadLine()) != null)
+            {
+                if (int.TryParse(line, out var result))
+                {
+                    fuel += result / 3 - 2;
+                }
+            }
+
+            Console.WriteLine($"Fuel requirements: {fuel}");
+        }
+
+        
+    }
+}
---
+++ C#/Solutions/Solution/Reader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Solution
+{
+    public class Reader
+    {
+        public static object ReadLines(string v)
+        {
+            throw new NotImplementedException();
+        }
+        public static StreamReader OpenStream(string argPath)
+        {
+            StreamReader file;
+            try
+            {
+                file = new StreamReader(argPath);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("The file does not exists on the disc.");
+                throw;
+            }
+
+            return file;
+        }
+    }
+}
