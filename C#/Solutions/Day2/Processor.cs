556d7e63f4a00c575f51e6bca22f5dc835cd516a
JakubCph <38160647+JakubCph@users.noreply.github.com>
2020-10-31 20:54:10 +0100

    Solved part 1 of Day2.

---
+++ C#/Solutions/Day2/Processor.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace Day2
+{
+    public class Processor
+    {
+        private const int Stop = 99;
+        private const int Add = 1;
+        private const int Multiply = 2;
+        private const int Step = 4;
+
+        public static int[] Process(int[] opcode)
+        {
+            int current = 0;
+            while(current < opcode.Length && opcode[current] != Stop)
+            {
+                if(opcode[current] == Add)
+                {
+                    executeAdd(opcode, current);
+                }
+                if(opcode[current] == Multiply)
+                {
+                    executeMultiply(opcode,current);
+                }
+
+                current += Step;
+            }
+
+            return opcode;
+        }
+
+        /// <summary>
+        /// Execute multiply operations on operands at positions indicated by opcode values
+        /// at locations +1 and +2 from current. Write to location at position indicated by opcode at current +3.
+        /// </summary>
+        private static void executeMultiply(int[] opcode, int current)
+        {
+            var a = opcode[opcode[current + 1]];
+            var b = opcode[opcode[current + 2]];
+            opcode[opcode[current + 3]] = a * b;
+
+            Console.WriteLine("Adding...");
+        }
+
+        /// <summary>
+        /// Add operation. Modify opcode in place. Gets numbers from locations pointed by the 
+        /// opcode value at locations +1 and +2 from current. Writes result at location pointed by 
+        /// opcode value at location +3 from current.
+        /// </summary>
+        private static void executeAdd(int[] opcode, int current)
+        {
+            var a = opcode[opcode[current + 1]];
+            var b = opcode[opcode[current + 2]];
+            opcode[opcode[current + 3]] = a + b;
+
+            Console.WriteLine("Multiplying...");
+        }
+    }
+}
---
+++ C#/Solutions/Day2/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day2
+{
+    class Solution
+    {
+        private const string Path = "Input.txt";
+
+        static void Main(string[] args)
+        {
+            var file = File.ReadAllText(Path);
+            var opcode = file.Split(',').Select(s => int.Parse(s)).ToArray();
+
+            setup(opcode);
+
+            var result = Processor.Process(opcode);
+
+            Console.WriteLine($"Value at [0]: {opcode[0]}");
+        }
+
+        private static void setup(int[] opcode)
+        {
+            opcode[1] = 12;
+            opcode[2] = 2;
+        }
+    }
+}
