1c3845a56321474b58b98c62150fe2903405f491
JakubCph <38160647+JakubCph@users.noreply.github.com>
2020-11-22 16:56:07 +0100

    Modified PasswordCombinator to check for double digits occuring only once.

--- C#/Solutions/Day4/PasswordCombinator.cs
+++ C#/Solutions/Day4/PasswordCombinator.cs
@@ -4,6 +4,8 @@ namespace Day4
 {
     internal class PasswordCombinator
     {
+        const int DIGIT_REPEATED_ONCE = 1;
+
         /// <summary>
         /// Calculated number of all possible combinations
         /// </summary>
@@ -12,7 +14,7 @@ namespace Day4
             var combinations = 0;
             for (int passPhrase = mIN; passPhrase <= mAX; passPhrase++)
             {
-                if (isIncreasing(passPhrase) && has2AdjacentDigits(passPhrase))
+                if (isIncreasing(passPhrase) && hasDouble(passPhrase))
                 {
                     combinations++;
                 }
@@ -21,17 +23,26 @@ namespace Day4
             return combinations;
         }
 
-        private static bool has2AdjacentDigits(int passPhrase)
+        private static bool hasDouble(int passPhrase)
         {
             var passString = passPhrase.ToString();
+            var repeatCounter = 0;
             for (int i = 0; i < passString.Length - 1; i++)
             {
                 if(passString[i+1] == passString[i])
                 {
+                    repeatCounter++;
+                }
+                else if(repeatCounter == DIGIT_REPEATED_ONCE)
+                {
                     return true;
                 }
+                else// digits different and repeatCounter NOT DIGIT_REPEATED_ONCE
+                {
+                    repeatCounter = 0;
+                }
             }
-            return false;
+            return repeatCounter == DIGIT_REPEATED_ONCE;// check if double at the end of passCode
         }
 
         private static bool isIncreasing(int passPhrase)
