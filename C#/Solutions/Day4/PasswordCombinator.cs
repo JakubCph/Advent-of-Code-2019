6ea49bdda2142213764cd6fb26e6acaef51188c1
JakubCph <38160647+JakubCph@users.noreply.github.com>
2020-11-22 16:03:39 +0100

    Implemented PaswordCombinator which computes all possible combinations of passwords given restrictions in Readme.txt.

---
+++ C#/Solutions/Day4/PasswordCombinator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Day4
+{
+    internal class PasswordCombinator
+    {
+        /// <summary>
+        /// Calculated number of all possible combinations
+        /// </summary>
+        internal static int CountCombinations(int mIN, int mAX)
+        {
+            var combinations = 0;
+            for (int passPhrase = mIN; passPhrase <= mAX; passPhrase++)
+            {
+                if (isIncreasing(passPhrase) && has2AdjacentDigits(passPhrase))
+                {
+                    combinations++;
+                }
+            }
+
+            return combinations;
+        }
+
+        private static bool has2AdjacentDigits(int passPhrase)
+        {
+            var passString = passPhrase.ToString();
+            for (int i = 0; i < passString.Length - 1; i++)
+            {
+                if(passString[i+1] == passString[i])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static bool isIncreasing(int passPhrase)
+        {
+            var passString = passPhrase.ToString();
+            for (int i = 0; i < passString.Length - 1; i++)
+            {
+                if (passString[i + 1] < passString[i]) 
+                {
+                    return false;             
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ C#/Solutions/Day4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MIN = 256310;
+            const int MAX = 732736;
+
+
+            var count = PasswordCombinator.CountCombinations(MIN, MAX);
+
+            Console.WriteLine($"Number of all combinations is: {count}.");
+        }
+    }
+}
